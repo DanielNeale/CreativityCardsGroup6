135db1bef2b0610a100ccbe0a65108d55827ffcf
DanielNeale <danielneale01@gmail.com>
2019-11-15 16:47:18 +0000

    Ocean man take me by the hand

---
+++ Lifes a Beach/Assets/Scripts/WaterMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMovement : MonoBehaviour
+{
+    Mesh water;
+    Vector3[] verticies;
+    public float waveRange;
+    public float waveSpeed;
+    float[] targetHeight;
+
+    void Start()
+    {
+        water = GetComponent<MeshFilter>().mesh;
+        verticies = water.vertices;
+        targetHeight = new float[verticies.Length];
+
+        for (int i = 0; i < targetHeight.Length; i++)
+        {
+            targetHeight[i] = Random.Range(-waveRange, waveRange);
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < verticies.Length; i++)
+        {
+
+            if (targetHeight[i] > verticies[i].y)
+            {
+                verticies[i].y += waveSpeed;
+
+                if (targetHeight[i] < verticies[i].y)
+                {
+                    targetHeight[i] = Random.Range(-waveRange, waveRange);
+                }
+            }
+
+            else if (targetHeight[i] < verticies[i].y)
+            {
+                verticies[i].y -= waveSpeed;
+
+                if (targetHeight[i] > verticies[i].y)
+                {
+                    targetHeight[i] = Random.Range(-waveRange, waveRange);
+                }
+
+            }
+
+            water.vertices = verticies;
+            water.RecalculateNormals();
+        }
+    }
+}
