f3633e6f3d0a99904d52a1133776c72312544a23
KalvinMalloch <km230683@falmouth.ac.uk>
2019-11-18 19:54:32 +0000

    Added the beep sound to the metal detector when you get close to dig spots.

--- Lifes a Beach/Assets/Scripts/ClosestDig.cs
+++ Lifes a Beach/Assets/Scripts/ClosestDig.cs
@@ -7,13 +7,40 @@ public class ClosestDig : MonoBehaviour
     public GameObject closestDigObj;
     public float closestDigDis;
     public float soundIntensity;
+	public float beepDelay = 10000f;
+	public bool beenPlayed = false;
+	public AudioSource detectSource;
+	public AudioClip detectBeep;
+
+	void Start()
+	{
+	}
 
     void Update()
     {
+		BeepIntensify();
 
         if (closestDigObj != null)
         {
             closestDigDis = Vector3.Distance(closestDigObj.transform.position, transform.position);
         }
     }
+	
+	void BeepIntensify()
+	{
+		if (closestDigDis < 5) {
+			StartCoroutine(startDetect());
+		}
+	}
+	
+	IEnumerator startDetect()
+	{
+		if (beenPlayed == false) 
+		{
+			beenPlayed = true;
+			detectSource.PlayOneShot(detectBeep);
+			yield return new WaitForSeconds(1);
+			beenPlayed = false;
+		}
+	}
 }
