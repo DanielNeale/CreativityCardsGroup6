c13a994a23dae98542cf532e287ccf9ee4a93642
toby <ta228737@falmouth.ac.uk>
2019-11-20 11:36:51 +0000

    Created child spawning and developed level.

---
+++ Lifes a Beach/Assets/Scripts/ChildSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildSpawner : MonoBehaviour
+{
+    public GameObject[] spawners;
+
+    public GameObject childPrefab;
+
+    void Start()
+    {
+        StartCoroutine(spawn());
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator spawn()
+    {
+        yield return new WaitForSeconds(50f);
+        int random = Random.Range(0, 4);
+        Vector3 _position = spawners[random].transform.position;
+        Instantiate(childPrefab, _position, Quaternion.identity);
+        StartCoroutine(spawn());
+    }
+}
