6d6fd4f4ed6d03dfb59ecf375777c1721284ef46
giblets2k <giblets2k@gmail.com>
2019-11-21 21:20:34 +0000

    Cleaned up main scene and began editing digging to work with inventory

--- Lifes a Beach/Assets/Assets/Ignore this it's not for you/Inventory/Scripts/Item.cs
+++ Lifes a Beach/Assets/Assets/Ignore this it's not for you/Inventory/Scripts/Item.cs
@@ -5,5 +5,31 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public ItemObject item;
-    
+
+    public ItemObject item1;
+    public ItemObject item2;
+    public ItemObject item3;
+    public ItemObject item4;
+    public ItemObject item5;
+    public ItemObject item6;
+    public ItemObject item7;
+    public ItemObject item8;
+    public ItemObject item9;
+    public ItemObject item10;
+    public ItemObject item11;
+    public ItemObject item12;
+    public ItemObject item13;
+    public ItemObject item14;
+    public ItemObject item15;
+    public ItemObject item16;
+    public ItemObject item17;
+    public ItemObject item18;
+    public ItemObject item19;
+    public ItemObject item20;
+
+    public void Start()
+    {
+        item = 
+    }
+
 }
--- Lifes a Beach/Assets/Scripts/DogDigging.cs
+++ Lifes a Beach/Assets/Scripts/DogDigging.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class DogDigging : MonoBehaviour
 {
+    public InventoryScript inventory;
+
     void OnTriggerStay(Collider other)
     {
-        if ((other.gameObject.tag == "DigSpot") &&(Input.GetKey(KeyCode.RightShift)))
+        if ((other.gameObject.tag == "DigSpot") &&(Input.GetKeyDown(KeyCode.RightShift)))
         {
+            var item = other.GetComponent<Item>();
+            if (item)
+            {
+                inventory.AddItem(item.item, 1);
+                Destroy(other.gameObject);
+            }
             print("Got Treasure!");
             Destroy(other.gameObject);
         }
+    }
+
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Clear();
     }
 }
