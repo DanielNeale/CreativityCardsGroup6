671c2ed4dc753df913c14ea04c7f0b9125613f7c
DanielNeale <danielneale01@gmail.com>
2019-11-21 15:44:21 +0000

    sun and shit

--- Lifes a Beach/Assets/Scripts/SkyboxCont.cs
+++ Lifes a Beach/Assets/Scripts/SkyboxCont.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class SkyboxCont : MonoBehaviour
 {
     public Material sky;
+    public Transform  sun;
     Color32 dawn;
     Color32 day;
     Color32 dusk;
@@ -15,6 +16,8 @@ public class SkyboxCont : MonoBehaviour
 
     void Start()
     {
+        sun.eulerAngles = new Vector3(130, sun.eulerAngles.y, sun.eulerAngles.z);
+
         dawn = new Color32(56, 65, 80, 128);
         day = new Color32(60, 93, 145, 128);
         dusk = new Color32(255, 63, 35, 128);
@@ -32,14 +35,16 @@ public class SkyboxCont : MonoBehaviour
 
         if(SceneManager.GetActiveScene().name == "MainScene2")
         {
-            if (changes == 0)
+            if (changes == 0 && sun.eulerAngles.x < 130)
             {
                 sky.color = Color.Lerp(dawn, day, (5.0f - changeTime) / 5.0f);
+                changeTime -= Time.deltaTime;
             }
 
-            if (changes == 1)
+            if (changes == 1 && sun.eulerAngles.x < 45)
             {
                 sky.color = Color.Lerp(day, dusk, (5.0f - changeTime) / 5.0f);
+                changeTime -= Time.deltaTime;
             }
 
             if (tillChange <= 0)
@@ -48,8 +53,7 @@ public class SkyboxCont : MonoBehaviour
                 changeTime = 5.0f;
                 tillChange = 600;
             }
-
-            changeTime -= Time.deltaTime;
+           
             tillChange -= Time.deltaTime;
         }        
     }
