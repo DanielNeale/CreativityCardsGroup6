e981d7ea36bf146715a2435785bcb42287d0fb92
KalvinMalloch <56155924+KalvinMalloch@users.noreply.github.com>
2019-11-21 15:41:08 +0000

    Fixing commit issues.

--- Lifes a Beach/Assets/Scripts/DialogueScript.cs
+++ Lifes a Beach/Assets/Scripts/DialogueScript.cs
@@ -9,34 +9,55 @@ public class DialogueScript : MonoBehaviour
     public GameObject shopkeeperFriendly;
     public GameObject shopkeeperNasty;
     public GameObject dialogueCanvas;
+    public float dialogueFadeValue;
+    public bool dialogueMenuActive;
     public bool Discount;
 
     void Start()
     {
         Discount = false;
+        dialogueMenuActive = true;
+    }
+    void Update()
+    {
+        dialogueFade();
     }
     public void Friendly()
     {
         shopkeeperFriendly.SetActive(true);
         shopkeeperNasty.SetActive(false);
         shopkeeperFlirt.SetActive(false);
-        dialogueCanvas.SetActive(false);
+        dialogueMenuActive = false;
     }
-
     public void Flirt()
     {
         shopkeeperFlirt.SetActive(true);
         shopkeeperNasty.SetActive(false);
         shopkeeperFriendly.SetActive(false);
-        dialogueCanvas.SetActive(false);
-        Discount = true;
+        dialogueMenuActive = false;
     }
-
     public void Nasty()
     {
         shopkeeperNasty.SetActive(true);
         shopkeeperFriendly.SetActive(false);
         shopkeeperFlirt.SetActive(false);
-        dialogueCanvas.SetActive(false);
+        dialogueMenuActive = false;
+        Discount = true;
+    }
+    public void dialogueFade()
+    {
+        dialogueCanvas.GetComponent<CanvasGroup>().alpha = dialogueFadeValue;
+        if (dialogueMenuActive == true)
+        {
+            dialogueFadeValue = 1;
+        }
+        if (dialogueMenuActive == false)
+        {
+            dialogueFadeValue -= Time.deltaTime;
+            if (dialogueFadeValue <= 0)
+            {
+                dialogueCanvas.SetActive(false);
+            }
+        }
     }
 }
