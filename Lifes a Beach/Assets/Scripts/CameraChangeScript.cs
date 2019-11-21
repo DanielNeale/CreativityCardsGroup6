93e3a1c2053314dd153e5517eeef272441bd94e4
KalvinMalloch <56155924+KalvinMalloch@users.noreply.github.com>
2019-11-21 14:42:31 +0000

    Fixing merge Issue

--- Lifes a Beach/Assets/Scripts/CameraChangeScript.cs
+++ Lifes a Beach/Assets/Scripts/CameraChangeScript.cs
@@ -12,6 +12,10 @@ public class CameraChangeScript : MonoBehaviour
     public GameObject nose;
     public GameObject shopkeeperAllMenus;
     public GameObject goToShopMenu;
+    public float ShopAllMenusFadeValue;
+    public bool ShopAllMenusActive;
+    public float ToShopMenuFadeValue;
+    public bool ToShopMenuActive;
 
 
     private void Start()
@@ -19,13 +23,18 @@ public class CameraChangeScript : MonoBehaviour
         vCam.SetActive(false);
         vCam2.SetActive(false);
     }
-
+    void Update()
+    {
+        shopAllMenusFade();
+        ToShopMenusFade();
+    }
     public void LookAtItems()
     {
         vCam.SetActive(false);
         vCam2.SetActive(true);
-        shopkeeperAllMenus.SetActive(false);
         goToShopMenu.SetActive(true);
+        ShopAllMenusActive = false;
+        ToShopMenuActive = true;
     }
     public void LookAtShop()
     {
@@ -33,23 +42,77 @@ public class CameraChangeScript : MonoBehaviour
         vCam2.SetActive(false);
         shopkeeperAllMenus.SetActive(true);
         goToShopMenu.SetActive(false);
+        ShopAllMenusActive = true;
+        ToShopMenuActive = false;
     }
-
+    public void shopAllMenusFade()
+    {
+        shopkeeperAllMenus.GetComponent<CanvasGroup>().alpha = ShopAllMenusFadeValue;
+        if (ShopAllMenusActive == true)
+        {
+            if (ShopAllMenusFadeValue < 1)
+            {
+                ShopAllMenusFadeValue += Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (ShopAllMenusFadeValue > 0)
+            {
+                ShopAllMenusFadeValue -= Time.deltaTime;
+            }
+        }
+        if (ShopAllMenusFadeValue <= 0)
+        {
+            shopkeeperAllMenus.SetActive(false);
+        }
+    }
+    public void ToShopMenusFade()
+    {
+        goToShopMenu.GetComponent<CanvasGroup>().alpha = ToShopMenuFadeValue;
+        if (ToShopMenuActive == true)
+        {
+            if (ToShopMenuFadeValue < 1)
+            {
+                ToShopMenuFadeValue += Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (ToShopMenuFadeValue > 0)
+            {
+                ToShopMenuFadeValue -= Time.deltaTime;
+            }
+        }
+        if (ToShopMenuFadeValue <= 0)
+        {
+            goToShopMenu.SetActive(false);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
-        vCam.SetActive(true);
-        player.GetComponent<MeshRenderer>().enabled = false;
-        nose.GetComponent<MeshRenderer>().enabled = false;
-        metalDetector.GetComponent<MeshRenderer>().enabled = false;
-       
-
+        if (other.gameObject.tag == "Player")
+        {
+            vCam.SetActive(true);
+            player.GetComponent<MeshRenderer>().enabled = false;
+            nose.GetComponent<MeshRenderer>().enabled = false;
+            metalDetector.GetComponent<MeshRenderer>().enabled = false;
+            ShopAllMenusActive = true;
+            shopkeeperAllMenus.SetActive(true);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        vCam.SetActive(false);
-        player.GetComponent<MeshRenderer>().enabled = true;
-        nose.GetComponent<MeshRenderer>().enabled = true;
-        metalDetector.GetComponent<MeshRenderer>().enabled = true;
+        if (other.gameObject.tag == "Player")
+        {
+            vCam.SetActive(false);
+            vCam2.SetActive(false);
+            player.GetComponent<MeshRenderer>().enabled = true;
+            nose.GetComponent<MeshRenderer>().enabled = true;
+            metalDetector.GetComponent<MeshRenderer>().enabled = true;
+            ShopAllMenusActive = false;
+            ToShopMenuActive = false;
+        }
     }
 }
