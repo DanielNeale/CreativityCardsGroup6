8395d96b35d350128c4edee1ad5d8ede00d36e3e
DanielNeale <danielneale01@gmail.com>
2019-11-21 21:36:53 +0000

    Finished item spawning

---
+++ Lifes a Beach/Assets/Scripts/IsItemColliding.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsItemColliding : MonoBehaviour
+{
+    void OnTriggerEnter(Collider hit)
+    {
+        if (!hit.transform.CompareTag("Ground"))
+        {
+            transform.parent.GetComponent<TreasureSpawner>().collidingItems += 1;
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Lifes a Beach/Assets/Scripts/TreasureSpawner.cs
+++ Lifes a Beach/Assets/Scripts/TreasureSpawner.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class TreasureSpawner : MonoBehaviour
 {
-    GameObject item;
-    int count;
+    public GameObject item;
+    public int count;
+    public int collidingItems;
+
     void Start()
     {
         for (int i = 0; i < count; i++)
         {
+            float newX = Random.Range(-37, 43);
+            float newZ = Random.Range(-25, 50);
+            Vector3 position = new Vector3(newX, 0, newZ);
+            GameObject newItem = Instantiate(item, position, this.transform.rotation, this.transform);
 
+            i -= collidingItems;
+            collidingItems = 0;
         }
-    }
-
-    void Update()
-    {
-        
     }
 }
