ab3938734475ec695fc9d80d32c5b815ee62cd2b
DanielNeale <danielneale01@gmail.com>
2019-11-22 00:31:50 +0000

    random spawning finished

--- Lifes a Beach/Assets/Scripts/IsItemColliding.cs
+++ Lifes a Beach/Assets/Scripts/IsItemColliding.cs
@@ -6,7 +6,7 @@ public class IsItemColliding : MonoBehaviour
 {
     void OnTriggerEnter(Collider hit)
     {
-        if (!hit.transform.CompareTag("Ground"))
+        if (!(hit.transform.CompareTag("Ground") || hit.transform.CompareTag("Dog") || hit.transform.CompareTag("Player") || hit.transform.CompareTag("MetalDetector")))
         {
             transform.parent.GetComponent<TreasureSpawner>().collidingItems += 1;
             Destroy(this.gameObject);
